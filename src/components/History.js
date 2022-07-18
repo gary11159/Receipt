f1968c502aec7dd175b9880d99b2e70d8f5a8ce7
Gary <gary11159@gmaill.com>
2022-07-18 08:53:15 +0800

    update

--- src/components/History.js
+++ src/components/History.js
@@ -238,6 +238,7 @@ function History(props) {
                             onChange={(date) => dateChange(date)}
                         />-
                         <DatePicker
+                            id="endDate"
                             selected={endDate}
                             locale={locale}
                             dateFormat="yyyy-MM"
