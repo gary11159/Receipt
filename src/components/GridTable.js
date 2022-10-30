0aa8cfa5a32ac4740c8f2657a6ff80b2e0fb4f6e
EX_STICH\gary1 <gary11159@gmail.com>
2022-10-30 20:37:26 +0800

    update

--- src/components/GridTable.js
+++ src/components/GridTable.js
@@ -29,7 +29,7 @@ const GridTable = (props) => {
     }
 
     const months = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
-    const days = ['一', '二', '三', '四', '五', '六', '日']
+    const days = ['日','一', '二', '三', '四', '五', '六']
     const locale = {
         localize: {
             day: n => days[n],
